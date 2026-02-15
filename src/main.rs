70213cb6732c4fcb1bc134b9c30b98c8bb89f33c
BitorqubitT <thierryars2@hotmail.com>
2026-02-15 16:12:00 +0100

    use from_uri for images to make dynamic paths work

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,7 @@ struct MyApp {
     height: u32,
     image_size: f32,
     zoom_level: i32,
+    image_path: String,
 }
 
 impl Default for MyApp {
@@ -30,15 +31,13 @@ impl Default for MyApp {
             height: 180,
             image_size: 30.,
             zoom_level: 100,
+            image_path: "file://assets/ferris.png".to_owned(),
         }
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-
-        let mut image_location = "../assets/ferris.png";
-
 
         //let id = Id::new("my_side_panel");
         egui::TopBottomPanel::top("my_top_panel").show(ctx, |ui| {
@@ -57,7 +56,7 @@ impl eframe::App for MyApp {
                 ui.menu_button("Options", |ui| {
                     if ui.button("change ferris").clicked() {
                         println!("Options 1 clicked");
-                        image_location = "../assets/ferris2.png";
+                        self.image_path = "file://assets/ferris2.png".to_owned();
                     }
                     ui.menu_button("More options", |ui|{
                         if ui.button("More Options 1").clicked() {
@@ -88,7 +87,7 @@ impl eframe::App for MyApp {
             //        .corner_radius(10),
             //);
 
-            ui.add(egui::Image::from_uri(image_location) // Use from_uri for dynamic paths
+            ui.add(egui::Image::from_uri(&self.image_path) // Use from_uri for dynamic paths
                 .max_width(self.image_size)
                 .corner_radius(10)
             );
