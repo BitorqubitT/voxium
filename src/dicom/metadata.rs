b0df641cb875bf363e760b152e1a4974eb9addc6
BitorqubitT <thierryars2@hotmail.com>
2026-06-28 19:30:37 +0200

    added default

--- src/dicom/metadata.rs
+++ src/dicom/metadata.rs
@@ -1,3 +1,5 @@
+use std::fs::Metadata;
+
 pub struct MetaData {
     pub patient_id: Option<String>,
     pub patient_name: Option<String>,
@@ -5,4 +7,17 @@ pub struct MetaData {
     pub pixel_representation: Option<u16>,
     pub bits_allocated: Option<u16>,
     pub photometric_interpretation: Option<String>,
+}
+
+impl Default for MetaData {
+    fn default() -> Self {
+        MetaData {
+            patient_id: None,
+            patient_name: None,
+            patient_weight: None,
+            pixel_representation: None,
+            bits_allocated: None,
+            photometric_interpretation: None,
+        }
+    }
 }
